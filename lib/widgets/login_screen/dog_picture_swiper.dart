9b9ead72ea5a5c8bfd435041fbed76cfa2bb9f7c
JoGeumJu <ccho8702@naver.com>
2023-04-16 22:24:08 +0900

    feat. Login_Screen UI#1 - swiper 추가

--- lib/widgets/login_screen/dog_picture_swiper.dart
+++ lib/widgets/login_screen/dog_picture_swiper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class DogPictureSwiper extends StatelessWidget {
   const DogPictureSwiper({super.key});
@@ -7,27 +8,61 @@ class DogPictureSwiper extends StatelessWidget {
   Widget build(BuildContext context) {
     return Positioned(
       top: 0,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            '주인아, 멍해?',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-            ),
+      width: MediaQuery.of(context).size.width,
+      child: CarouselSlider(
+          options: CarouselOptions(
+            height: 240,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 5),
+            autoPlayAnimationDuration: const Duration(milliseconds: 1200),
+            autoPlayCurve: Curves.fastOutSlowIn,
           ),
-          Transform.translate(
-            offset: const Offset(0, -15),
-            child: Image.asset(
-              'assets/images/dog_pictures/stand_dog.png',
-              width: 225,
-              height: 210,
+          items: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text(
+                  '주인아, 멍해?',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Transform.translate(
+                  offset: const Offset(0, -8),
+                  child: Image.asset(
+                    'assets/images/dog_pictures/stand_dog.png',
+                    width: 200,
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
             ),
-          ),
-        ],
-      ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 32),
+                const Text(
+                  '멍멍아, 멍해?',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Transform.translate(
+                  offset: const Offset(0, -8),
+                  child: Image.asset(
+                    'assets/images/dog_pictures/lay_dog.png',
+                    width: 180,
+                  ),
+                ),
+              ],
+            ),
+          ]),
     );
   }
 }
