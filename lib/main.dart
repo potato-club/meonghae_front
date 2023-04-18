9743d766c9cf0ea5b03981f4a68034cf590c8995
JoGeumJu <ccho8702@naver.com>
2023-04-18 10:30:12 +0900

    feat. Select_Screen UI 구현완료 #5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-import 'screens/login_screen.dart';
+import 'package:meonghae_front/screens/select_screen.dart';
 
 void main() async {
   // 카카오 앱의 클라이언트 ID를 설정
@@ -12,6 +12,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginScreen());
+    return const MaterialApp(home: SelectScreen());
   }
 }
