41bc06493faf70d69d8b8bfed45125536b3c84d2
JoGeumJu <ccho8702@naver.com>
2023-04-21 02:37:47 +0900

    feat. 환경설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-import 'package:meonghae_front/screens/login_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:meonghae_front/screens/register_screen.dart';
 
 void main() async {
   // 카카오 앱의 클라이언트 ID를 설정
@@ -16,13 +16,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
 
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Meonghae?',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginScreen(),
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      home: RegisterScreen(),
     );
   }
 }
