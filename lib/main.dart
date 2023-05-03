53dd2339c0bffbb73ff8dab10def29245ad5af37
JoGeumJu <ccho8702@naver.com>
2023-05-03 20:14:15 +0900

    feat. Main_Screen UI #2 - 반응형 추가 - customColor 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:flutter/services.dart';
-import 'package:meonghae_front/screens/login_screen.dart';
+import 'package:meonghae_front/screens/main_screen.dart';
 
 void main() async {
   // 카카오 앱의 클라이언트 ID를 설정
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
-      home: LoginScreen(),
+      home: MainScreen(),
     );
   }
 }
