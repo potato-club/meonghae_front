503579cc8ce733e1042c71327b43dda0d736e70f
thdwo999@naver.com <thdwo999@naver.com>
2023-05-30 14:12:36 +0900

    start

---
+++ lib/screens/inquiry_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InquiryScreen extends StatelessWidget {
+  const InquiryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
