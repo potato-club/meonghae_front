bfde55c98f1b1e1d53898482198c38c0acaaf298
JoGeumJu <ccho8702@naver.com>
2023-07-20 18:40:38 +0900

    feat. CalendarSearchScreen Publishing #17

--- lib/widgets/calendar_search_screen/search_item_widget.dart
+++ lib/widgets/calendar_search_screen/search_item_widget.dart
@@ -24,37 +24,52 @@ class SearchItemWidget extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.only(
                   left: 14, top: 18, bottom: 28, right: 24),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: 5,
-                    height: 20,
-                    decoration: BoxDecoration(
-                        color: CustomColor.brown1,
-                        borderRadius: BorderRadius.circular(5)),
-                  ),
-                  const SizedBox(width: 8),
-                  const Text('오전 10:00',
-                      style: TextStyle(fontSize: 14, color: CustomColor.gray)),
-                  SizedBox(width: MediaQuery.of(context).size.width * 0.06),
-                  const Text.rich(TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: '멍멍이 ',
-                        style: TextStyle(fontSize: 14, color: CustomColor.gray),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 5,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            color: CustomColor.brown1,
+                            borderRadius: BorderRadius.circular(5)),
                       ),
-                      TextSpan(
-                        text: '예방주사',
-                        style:
-                            TextStyle(fontSize: 14, color: CustomColor.brown1),
-                      ),
-                      TextSpan(
-                        text: ' 맞는 날',
-                        style: TextStyle(fontSize: 14, color: CustomColor.gray),
-                      ),
+                      const SizedBox(width: 8),
+                      const Text('오전 10:00',
+                          style:
+                              TextStyle(fontSize: 14, color: CustomColor.gray)),
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.06),
+                      const Text.rich(TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '멍멍이의 ',
+                            style: TextStyle(
+                                fontSize: 14, color: CustomColor.gray),
+                          ),
+                          TextSpan(
+                            text: '예방주사',
+                            style: TextStyle(
+                                fontSize: 14, color: CustomColor.brown1),
+                          ),
+                          TextSpan(
+                            text: '',
+                            style: TextStyle(
+                                fontSize: 14, color: CustomColor.gray),
+                          ),
+                        ],
+                      )),
                     ],
-                  )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12),
+                    child: Text(
+                      '메모 내용 솰라솰라',
+                      style: TextStyle(fontSize: 13, color: CustomColor.gray),
+                    ),
+                  ),
                 ],
               ),
             ),
